ec71beb4408d33a1e768280c650baf17f1b3599c
Jiameng Fan <jmfan@bu.edu>
2020-02-13 17:29:28 -0500

    simple dataset

--- image_generation/CreateSimpleDataset.sh
+++ image_generation/CreateSimpleDataset.sh
@@ -2,5 +2,5 @@
 
 for i in 0 900 1800 2700 3600 4500 5400 6300 7200 8100 9000 9900 10800 11700 12600 13500 14400 15300 16200 17100 18000 18900 19800 20700
 do
-    ./../../blender/blender --background --python render_images.py -- --start_idx $i --properties_json data/simple_properties.json --max_objects 5
+    ./../../blender/blender --background --python render_images.py -- --start_idx $i --properties_json data/simple_properties.json --max_objects 5 --output_image_dir ../simple_dataset/images --output_scene_dir ../simple_dataset/scenes --output_scene_file ../simple_dataset/CLEVR_scenes.json
 done
