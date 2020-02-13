0f8ac5617fe1b0bfee30ad3a7af9ddb9e0cca632
Jiameng Fan <jmfan@bu.edu>
2020-02-13 17:26:38 -0500

    simple_dataset

---
+++ image_generation/CreateSimpleDataset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in 0 900 1800 2700 3600 4500 5400 6300 7200 8100 9000 9900 10800 11700 12600 13500 14400 15300 16200 17100 18000 18900 19800 20700
+do
+    ./../../blender/blender --background --python render_images.py -- --start_idx $i --properties_json data/simple_properties.json --max_objects 5
+done
